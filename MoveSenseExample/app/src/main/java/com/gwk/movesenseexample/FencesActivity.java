8731e50d89b68707486dcb8a440831bf59925f8a
Michinggun <michinggun@gmail.com>
2017-03-26 06:09:54 +0700

    Add Step Counter & Detector

--- MoveSenseExample/app/src/main/java/com/gwk/movesenseexample/FencesActivity.java
+++ MoveSenseExample/app/src/main/java/com/gwk/movesenseexample/FencesActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -45,11 +47,14 @@ public class FencesActivity extends AppCompatActivity implements MoveSenseReceiv
     @BindView(R.id.fencing_time) TextView tvTime;
     @BindView(R.id.fencing_current_time) TextView tvCurrentTime;
     @BindView(R.id.fencing_move) TextView tvMove;
+    @BindView(R.id.step_sensor) TextView tvStepSensor;
 
-    //Significant motion sensor
+    //Step Counter
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    //private TriggerEventListener mTriggerEventListener;
+    private int stepCounter = 0;
+    private int counterSteps = 0;
+    private int stepDetector = 0;
 
     private MoveSenseFences mMoveSenseFences;
     private MoveSenseReceiver mMoveSenseReceiver;
@@ -77,34 +82,37 @@ public class FencesActivity extends AppCompatActivity implements MoveSenseReceiv
         // Register the fence to receive callbacks.
         mMoveSenseFences.registerFences();
 
-
-        // Significant Motion
-//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
-//        mTriggerEventListener = new TriggerEventListener() {
-//            @Override
-//            public void onTrigger(TriggerEvent event) {
-//                for (float value : event.values) {
-//                    Log.i(TAG, event.sensor.getName() + ": " + value);
-//                }
-//            }
-//        };
-//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        // Step Counter
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-    }
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mSensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                switch (event.sensor.getType()) {
+                    case Sensor.TYPE_STEP_DETECTOR:
+                        stepDetector++;
+                        //tvStepSensor.setText(stepCounter + "\n" + stepDetector);
+                        break;
+                    case Sensor.TYPE_STEP_COUNTER:
+                        //Since it will return the total number since we registered we need to subtract the initial amount
+                        //for the current steps since we opened app
+                        if (counterSteps < 1) {
+                            // initial value
+                            counterSteps = (int) event.values[0];
+                        }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // mSensorManager.requestTriggerSensor(mTriggerEventListener, mSensor);
-    }
+                        // Calculate steps taken based on first counter value received.
+                        stepCounter = (int) event.values[0] - counterSteps;
+                        tvStepSensor.setText(stepCounter + " steps");
+                        break;
+                }
+            }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // mSensorManager.cancelTriggerSensor(mTriggerEventListener, mSensor);
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        }, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
